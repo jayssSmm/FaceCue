7b6aae758eb44a2f4b8106c82a13d3462c3d0b74
Samman jaiswal <214768065+jayssSmm@users.noreply.github.com>
2026-08-23 11:05:10 +0000

    handling postgres session from session pool

---
+++ app/db_DataHandling/getSession.py
@@ -0,0 +1,13 @@
+# db/dependencies.py
+from typing import AsyncGenerator
+from app.extension import AsyncSessionLocal
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
